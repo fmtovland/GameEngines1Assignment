7a5c117fd0a9be9efb8fdc49384aadf1aa6b4344
Liam McCormick <ljmc2000@gmail.com>
2019-12-06 18:23:41 +0000

    added script to control camera

---
+++ Assets/CameraControlScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControlScript : MonoBehaviour
+{
+    public float turnSpeed=100,speed=5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Time.deltaTime * Input.GetAxis("Horizontal") * speed
+                            ,0
+                            ,Time.deltaTime * Input.GetAxis("Vertical") * speed);
+
+        transform.Rotate(Time.deltaTime * Input.GetAxis("RVertical") * turnSpeed
+                        ,Time.deltaTime * Input.GetAxis("RHorizontal") * turnSpeed
+                        ,Time.deltaTime * (Input.GetAxis("LT")-Input.GetAxis("RT")) * turnSpeed);
+
+    }
+}
---
+++ Assets/TreeScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeScript : MonoBehaviour
+{
+    public enum Side {North, South, West, East, Below};
+    public Side side=Side.Below;
+
+    public IEnumerator spawn()
+    {
+        yield return new WaitForSeconds(1);
+        GameObject g = Instantiate(gameObject);
+    }
+
+    public IEnumerator grow()
+    {
+        while(transform.position.y < .2f)
+        {
+            transform.Translate(0,.02f,0);
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+//        StartCoroutine(spawn());
+//        StartCoroutine(grow());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
