91dfe7ce96753693641967a774d6eaafff29e37d
Liam McCormick <ljmc2000@gmail.com>
2019-12-06 18:59:49 +0000

    added vertical spawn limits for tree cells

--- Assets/TreeScript.cs
+++ Assets/TreeScript.cs
@@ -5,19 +5,30 @@ using UnityEngine;
 public class TreeScript : MonoBehaviour
 {
     public enum Side {North, South, West, East, Below};
-    public Side side=Side.Below;
+    public Side spawnSide=Side.Below;
+    public int[] limits = {4,4,4,4,10};
+    public int[] ancestors = {0,0,0,0,0};
 
     public IEnumerator spawn()
     {
         yield return new WaitForSeconds(1);
-        GameObject g = Instantiate(gameObject);
+
+        int sidenum=(int)Side.Below;
+
+        if(ancestors[sidenum]<limits[sidenum])
+        {
+            GameObject g = Instantiate(gameObject);
+            g.transform.SetParent(transform);
+            TreeScript t = g.GetComponent<TreeScript>();
+            t.ancestors[sidenum]+=1;
+        }
     }
 
     public IEnumerator grow()
     {
-        while(transform.position.y < .2f)
+        while(transform.localPosition.y < 1f)
         {
-            transform.Translate(0,.02f,0);
+            transform.Translate(0,.1f,0);
             yield return new WaitForSeconds(.1f);
         }
     }
@@ -25,8 +36,8 @@ public class TreeScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-//        StartCoroutine(spawn());
-//        StartCoroutine(grow());
+        StartCoroutine(spawn());
+        StartCoroutine(grow());
     }
 
     // Update is called once per frame
